0c5595b75b2c648d8ffc51c4ba07c5323e9cf701
xiepei <1005039110@qq.com>
2017-12-14 00:21:23 +0800

    本地mock接口

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,9 +1,6 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
-
-console.log(2323);
-
 module.exports = merge(prodEnv, {
     NODE_ENV: '"development"',
     API_ROOT: '"http://localhost:9090"'
